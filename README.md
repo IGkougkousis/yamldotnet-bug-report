70b7439871f1604f9879b0cc7a5a1f4c0cece3c1
IGkougkousis <iraklisgougousis@protonmail.com>
2022-02-25 17:44:26 +0100

    Add minimum reproducible example for bug report

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # yamldotnet-bug-report
 Minimum reproducible example for YamlDotNet bug report
+
+## How to run
+
+You need dotnet CLI 6 or higher.
+
+```
+cd YamlDotNetBugReport
+dotnet run
+```
+
+The output should look like this:
+```
+~~~ Trying to parse example1.yaml ~~~ 
+Employees: 3
+- Name: John Smith, Age: 30, Title: Software Developer
+- Name: John Smith, Age: 30, Title: Software Developer
+- Name: Mary Jane, Age: 30, Title: Software Developer 
+
+~~~ Done with example1.yaml ~~~
+~~~ Trying to parse example2.yaml ~~~ 
+Employees: 2
+- Name: John Smith, Age: 30, Title: Software Developer
+- Name: Mary Jane, Age: 30, Title: Software Developer
+
+~~~ Done with example2.yaml ~~~
+~~~ Trying to parse example3.yaml ~~~
+Failed parsing example3.yaml! Exception message:
+(Line: 9, Col: 5, Idx: 269) - (Line: 9, Col: 16, Idx: 280): Alias $swdev_30yo cannot precede anchor declaration
+```
---
+++ YamlDotNetBugReport/Program.cs
@@ -0,0 +1,65 @@
+﻿using YamlDotNet.Core;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace YamlDotNetBugReport
+{
+    public static class YamlReader<T>
+    {
+        public static T Read(string filename)
+        {
+            var deserializer = new DeserializerBuilder()
+                .WithNamingConvention(UnderscoredNamingConvention.Instance)
+                .IgnoreUnmatchedProperties()
+                .Build();
+
+            using StreamReader sr = new(filename);
+            var parser = new Parser(sr);
+            var mergingParser = new MergingParser(parser);
+            return deserializer.Deserialize<T>(mergingParser);
+        }
+    }
+
+    public class Employee
+    {
+        public string Name { get; init; } = string.Empty;
+        public string Title { get; init; } = string.Empty;
+        public int Age { get; init; }
+    }
+
+    public class Company
+    {
+        public Employee[] Employees { get; init; } = Array.Empty<Employee>();
+    }
+
+    public class Program
+    {
+        private static int Main()
+        {
+            for (int i = 1; i <= 3; i++)
+            {
+                string yaml = $"example{i}.yaml";
+
+                try
+                {
+                    Console.WriteLine($"~~~ Trying to parse {yaml} ~~~ ");
+
+                    Company company = YamlReader<Company>.Read(yaml);
+                    Console.WriteLine($"Employees: {company.Employees.Length}");
+                    foreach (Employee e in company.Employees)
+                        Console.WriteLine($"- Name: {e.Name}, Age: {e.Age}, Title: {e.Title}");
+
+                    Console.WriteLine();
+                    Console.WriteLine($"~~~ Done with {yaml} ~~~");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Failed parsing {yaml}! Exception message:");
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            return 0;
+        }
+    }
+}
